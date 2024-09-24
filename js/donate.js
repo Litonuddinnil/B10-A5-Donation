94790d92a0179d958feab25d9f3ed512714be3fd
Litonuddinnil <mdniloyhasan544@gmail.com>
2024-09-24 09:51:56 +0600

    complete history part

--- js/donate.js
+++ js/donate.js
@@ -1,22 +1,45 @@
 
-// NOakhali
+// Noakhali
+
 document.getElementById('donate-Noakhali').addEventListener('click',function(event){
     event.preventDefault();
+   
     const inputFieldValueNoakhali =getInputFieldValueById('input-data-Noakhali'); 
-    const donationAmount =getTextFieldValueById('input-data-Noakhali'); 
     
-    if(donationAmount === '' || isNaN(donationAmount) || donationAmount <= 0){
-        // add to money Noakhali Donation
-        const donateNoakhaliNumber =getTextFieldValueById('Money-Noakhali'); 
-        const totalDonateNoakhali = inputFieldValueNoakhali+donateNoakhaliNumber; 
-        document.getElementById('Money-Noakhali').innerText=totalDonateNoakhali; 
-        // current money sub new balance
-        const currentMoneyNumber = getTextFieldValueById('current-Balance');
-        const newBalance =currentMoneyNumber-inputFieldValueNoakhali;
-        document.getElementById('current-Balance').innerText=newBalance;
+    const donationAmount =Number(inputFieldValueNoakhali);  
+    // if(donationAmount > 0 && Number.isInteger(donationAmount)){
+    //     hiddenMode.classList.remove('hidden');
+    // }
+     
+    if( donationAmount > 0 && Number.isInteger(donationAmount) ){
+          // add to money Noakhali Donation
+          const donateNoakhaliNumber =getTextFieldValueById('Money-Noakhali'); 
+          const totalDonateNoakhali = inputFieldValueNoakhali+donateNoakhaliNumber; 
+          // current money sub new balance
+          const currentMoneyNumber = getTextFieldValueById('current-Balance');
+          const newBalance =currentMoneyNumber-inputFieldValueNoakhali;
+          if(newBalance>0){
+              
+              document.getElementById('Money-Noakhali').innerText=totalDonateNoakhali; 
+              document.getElementById('current-Balance').innerText=newBalance;
+          }
+           
+            else{
+                alert('Your current balance is low.');
+                hiddenMode.classList.add('hidden');
+                window.location.reload();
+              }
+          
+          // history donation
+          const p =document.createElement('p');
+          p.innerText =`${inputFieldValueNoakhali} Taka is ${NoakhaliTitle.innerText}
+           ${new Date()}`;
+          document.getElementById('NoakhaliHistory').appendChild(p);  
     } 
     else{
         alert('Not a valid Number for Donation');
+        hiddenMode.classList.add('hidden');
+        window.location.reload();
     }
 });
 
@@ -24,19 +47,35 @@ document.getElementById('donate-Noakhali').addEventListener('click',function(eve
 document.getElementById('donate-feni').addEventListener('click',function(event){
     event.preventDefault(); 
     const inputFieldValueFeni =getInputFieldValueById('input-data-Feni');
-    const donationAmount=getTextFieldValueById('input-data-Feni');
-    if(donationAmount === '' || isNaN(donationAmount) || donationAmount <= 0){
-       // add to money Feni Donation
-        const donateFeniNumber =getTextFieldValueById('Money-Feni'); 
-        const totalDonateFeni = inputFieldValueFeni+donateFeniNumber; 
-        document.getElementById('Money-Feni').innerText=totalDonateFeni; 
-        // current money sub new balance
-        const currentMoneyNumber = getTextFieldValueById('current-Balance');
-        const newBalance =currentMoneyNumber-inputFieldValueFeni;
-        document.getElementById('current-Balance').innerText=newBalance;
+    const donationAmount= Number(inputFieldValueFeni);
+     
+    if(donationAmount > 0 && Number.isInteger(donationAmount)){
+             // add to money Feni Donation
+             const donateFeniNumber =getTextFieldValueById('Money-Feni'); 
+            const totalDonateFeni = inputFieldValueFeni+donateFeniNumber; 
+            // current money sub new balance 
+            const currentMoneyNumber = getTextFieldValueById('current-Balance');
+            const newBalance =currentMoneyNumber-inputFieldValueFeni;
+            if(newBalance>0){
+                 
+                 document.getElementById('Money-Feni').innerText=totalDonateFeni; 
+                 document.getElementById('current-Balance').innerText=newBalance;
+            } 
+          else{
+            alert('Your current balance is low.');
+            hiddenMode.classList.add('hidden');
+            window.location.reload();
+          }
+        // donate feni history
+        const p =document.createElement('p');
+        p.innerText =`${inputFieldValueFeni}Taka is${ feniTitle.innerText}
+         ${new Date()}`;
+        document.getElementById('FeniHistory').appendChild(p);
     }
     else{
         alert('Not a valid Number for Donation');
+        hiddenMode.classList.add('hidden');
+        window.location.reload();
     }
 });
 
@@ -44,18 +83,34 @@ document.getElementById('donate-feni').addEventListener('click',function(event){
 document.getElementById('donate-quota').addEventListener('click',function(event){
     event.preventDefault(); 
     const inputFieldValuequota =getInputFieldValueById('input-data-quota');
-    const donationAmount=getTextFieldValueById('input-data-quota');
-    if(donationAmount === '' || isNaN(donationAmount) || donationAmount <= 0){
+    const donationAmount= Number(inputFieldValuequota);
+ 
+    if(donationAmount > 0 && Number.isInteger(donationAmount)){
        // add to money Feni Donation
         const donatequotaNumber =getTextFieldValueById('Money-quota'); 
         const totalDonatequota = inputFieldValuequota+donatequotaNumber; 
-        document.getElementById('Money-quota').innerText=totalDonatequota; 
         // current money sub new balance
         const currentMoneyNumber = getTextFieldValueById('current-Balance');
         const newBalance =currentMoneyNumber-inputFieldValuequota;
-        document.getElementById('current-Balance').innerText=newBalance;
+        if(newBalance>0){
+            
+            document.getElementById('Money-quota').innerText=totalDonatequota; 
+            document.getElementById('current-Balance').innerText=newBalance;
+        }
+        else{
+            alert('Your current balance is low.');
+            hiddenMode.classList.add('hidden');
+            window.location.reload();
+          }
+        // quota donation history
+          const p =document.createElement('p');
+        p.innerText =`${inputFieldValuequota} Taka is${quotaTitle.innerText}
+         ${new Date()}`;
+        document.getElementById('quotaHistory').appendChild(p);
     }
     else{
         alert('Not a valid Number for Donation');
+        hiddenMode.classList.add('hidden');
+        window.location.reload();
     }
-})
+}) 
--- js/history.js
+++ js/history.js
@@ -1,6 +1,4 @@
-//  function selectBtn(event){
-//     event.classList.add('hidden');
-//  }
+ 
 document.getElementById('btnHistory').addEventListener('click',function(){
      
     noakhaliSection.classList.add('hidden');
@@ -8,12 +6,22 @@ document.getElementById('btnHistory').addEventListener('click',function(){
     quotaSection.classList.add('hidden');
     btnHistory.classList.add('bg-btnColor');
     btnDonation.classList.remove('bg-btnColor');
+    
         
-})
+});
 document.getElementById('btnDonation').addEventListener('click',function(){
     noakhaliSection.classList.remove('hidden');
     feniSection.classList.remove('hidden');
     quotaSection.classList.remove('hidden');
     btnDonation.classList.add('bg-btnColor');
     btnHistory.classList.remove('bg-btnColor');
-})
+});
+document.getElementById('donate-Noakhali').addEventListener('click',function(){
+    NoakhaliHistory.classList.remove('hidden');
+});
+document.getElementById('donate-feni').addEventListener('click',function(){
+    FeniHistory.classList.remove('hidden');
+});
+document.getElementById('donate-quota').addEventListener('click',function(){
+    quotaHistory.classList.remove('hidden');
+});
